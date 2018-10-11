d40b803fedd03c50f2c4fee010cc5c2ba547854f
Ignacio <ignacioito@gmail.com>
2018-10-11 19:02:20 -0400

    commit

---
+++ fonctionsyahtzee.c
@@ -0,0 +1,55 @@
+//
+// TP03
+//
+// Test de fonctions
+//
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Jet Jet;
+struct Jet{
+	int des[5];
+};
+
+
+int yahtzee(Jet ig);
+
+
+
+int main(int argc, char const *argv[])
+{
+	Jet ig;
+
+	ig.des[0] = 6;
+	ig.des[1] = 6;
+	ig.des[2] = 3;
+	ig.des[3] = 6;
+	ig.des[4] = 6;
+
+
+	printf("test %d\n", yahtzee(ig));
+	
+
+	return 0;
+}
+
+
+int yahtzee(Jet ig){
+
+ 	
+	int yahtzee = 50;
+
+	
+	//for (int i = 0; i < 5; ++i)
+	//{
+		if((ig.des[0] == ig.des[1]) && (ig.des[1] == ig.des[2]) 
+			&& (ig.des[2]== ig.des[3]) && (ig.des[3]== ig.des[4])) 
+		{
+			//printf("Teste %d\n", yahtzee);
+			return 50;
+		}
+	//}
+	return 0;
+}
